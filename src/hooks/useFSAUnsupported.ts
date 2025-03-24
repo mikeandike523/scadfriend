4107c3fb045530b1aa2b45bbae50d0f1d73f8d88
Michael Sohnen <michaelsohnenacademic@gmail.com>
2025-03-24 15:04:33 -0400

    Fixed build errors.

--- src/hooks/useFSAUnsupported.ts
+++ src/hooks/useFSAUnsupported.ts
@@ -1,28 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 /**
  * Hook to check if the File System Access API is unsupported in the current browser
  */
 export default function useFSAUnsupported() {
-  const [isSupported, setIsSupported] = useState<boolean|null>(null);
+  const [isSupported, setIsSupported] = useState<boolean | null>(null);
 
   useEffect(() => {
     // List of required File System Access API functions
     const requiredFunctions = [
-      'showOpenFilePicker',
-      'showSaveFilePicker',
-      'showDirectoryPicker'
+      "showOpenFilePicker",
+      "showSaveFilePicker",
+      "showDirectoryPicker",
     ];
 
     // Check if all required functions are available on the window object
     const supported = requiredFunctions.every(
-      funcName => typeof (window as object as {
-        [funcName: string]: unknown | undefined
-      })[funcName] === 'function'
+      (funcName) =>
+        typeof (
+          window as object as {
+            [funcName: string]: unknown | undefined;
+          }
+        )[funcName] === "function"
     );
 
     setIsSupported(supported);
   }, []);
-const loaded = isSupported !== null;
-  return loaded && !isSupported
+
+  const loaded = isSupported !== null;
+  return loaded && !isSupported;
 }
