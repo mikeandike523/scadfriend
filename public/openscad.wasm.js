e81e35922e285e662cd249f720935a8738137cce
Michael Sohnen <michaelsohnenacademic@gmail.com>
2025-04-14 07:45:19 -0400

    more vercel compat testing

--- public/openscad.wasm.js
+++ public/openscad.wasm.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c895a87483d92b9ef1d68a92ad7eeb4f8f80d681c25890a611b6189a211cf085
-size 175664
+oid sha256:f1cf2c8c4dc29a149e8eac02f1ecf882a413d626793b10a519de9d3cfdd6fb01
+size 175609
